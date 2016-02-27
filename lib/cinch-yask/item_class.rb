62517bab5957131f8da35b1bf5a765fc0fb69538
bbourqu <brett@bourqu.in>
2016-02-26 22:26:06 -0600

    Adding base classes for item, player, and item class

---
+++ lib/cinch-yask/item_class.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# Class:    Item Class
+# Purpose:  Base Item Class
+# Location: https://github.com/Cinch-RPGPlugins/cinch-yask/lib/item_Class
+#:nodoc:
+
+class Item_Class
+
+end
+
---
+++ lib/cinch-yask/player.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# Class: Player
+# Purpose: Represents a Base Player
+# Location: https://github.com/Cinch-RPGPlugins/cinch-yask
+#:nodoc:
+class Player
+  @name
+  @head
+  @eyes
+  @neck
+  @body
+  @torso
+  @legs
+  @right_hand
+  @left_hand
+  @hands
+  @feet
+  @gold
+  @waist
+  @shoulders
+  @wrists
+  @additional
+end
